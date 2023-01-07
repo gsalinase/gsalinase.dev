f614dfbf26423fbd0ef34ed834d03533e2635a94
Gonzalo Salinas <gsalinase91@gmail.com>
2023-01-07 09:02:05 -0300

    [GSP-001] WIP navigation

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navigation">
+    {{ props.items }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+    default: () => ([])
+  }
+})
+</script>
+
+<style lang="css" scoped>
+
+
+@media screen and (min-width: 600px) {
+  .navigation {
+    position: sticky;
+    top:0;
+    left: 0;
+    bottom: 0;
+    min-width: 300px;
+    max-height: 100vh;
+    background-color: var(--red);
+  }
+}
+</style>
