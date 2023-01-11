3fc6445145b935244acd4e29b98bd1069d2440f3
Gonzalo Salinas <gsalinase91@gmail.com>
2023-01-11 00:33:35 -0300

    [GSP-001] WIP navigation content

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,21 +1,105 @@
 <template>
-  <div class="navigation">
-    {{ props.items }}
+  <div class="navigation" :class="isNavigationOpen ? 'navigation-open' : null">
+    <!-- Hamburger/close button -->
+    <a class="navigation-toggle" href="#" @click.stop="toggleNav">
+      <span class="navigation-bar bar-1"></span> 
+      <span class="navigation-bar bar-2"></span>
+      <span class="navigation-bar bar-3"></span>
+    </a>
+
+    <!-- Welcome message -->
+    <div class="navigation-message">
+      {{ welcomeMessage }}
+    </div>
+
+    <!-- Links -->
+    <ul v-if="isNavigationOpen" class="navigation-list">
+      <li class="navigation-item">{{ props.items }}</li>
+    </ul>
+
+    <div class="navigation-footer">
+      <div class="navigation-logo">
+        <slot name="logo"></slot>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+// Imports
+import { ref } from 'vue'
+
+// Props
 const props = defineProps({
   items: {
     type: Array,
     required: true,
     default: () => ([])
+  },
+  welcomeMessage: {
+    type: String,
+    required: false,
+    default: 'Default message!'
   }
+})
+
+// Variables
+const isNavigationOpen = ref(false)
+
+// Methods
+const toggleNav = (e) => {
+  console.log(e)
+}
+
+// Expose variables and functions
+defineExpose({
+  isNavigationOpen,
+  toggleNav,
 })
 </script>
 
 <style lang="css" scoped>
+.navigation {
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
+  padding: 2rem 1rem;
+  background-color: var(--red);
+}
 
+.navigation-message {
+  min-width: max-content;
+  font-family: var(--motserrat);
+  font-size: var(--size-md);
+  color: var(--white);
+  rotate: -90deg;
+}
+
+.navigation-toggle {
+  display: flex;
+  flex-direction: column;
+
+}
+
+.navigation-bar {
+  display: inline-block;
+  height: 3px;
+  background-color: var(--white);
+  margin-bottom: 6px;
+}
+
+.bar-1, 
+.bar-3 {
+  width: 1.2rem;
+}
+
+.bar-2 {
+  width: 2rem;
+}
+
+.bar-3 {
+
+}
 
 @media screen and (min-width: 600px) {
   .navigation {
@@ -23,9 +107,22 @@ const props = defineProps({
     top:0;
     left: 0;
     bottom: 0;
+    max-height: calc(-64px + 100vh);
+    justify-content: space-between;
+  }
+
+  .navigation-open {
+    min-width: 200px;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .navigation {
+
+  }
+
+  .navigation-open {
     min-width: 300px;
-    max-height: 100vh;
-    background-color: var(--red);
   }
 }
 </style>
